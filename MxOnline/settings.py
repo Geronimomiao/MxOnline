367e4c75a8bbe8a555760d5cf9df9113af138ed2
wsm <1030057982@qq.com>
2019-01-23 13:15:14 +0800

    model 信息设计完成

--- MxOnline/settings.py
+++ MxOnline/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
-    'courses'
+    'courses',
+    'operation',
+    'organization'
 ]
 
 MIDDLEWARE = [
